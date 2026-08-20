adce31c0c484fd2540a067283b988b080b5a35a9
Neel Bose <neelbosen.jr11@gmail.com>
2026-08-20 06:28:10 +0000

    added backend and provider code and perfromed terrafom init across both dev and prod environments

---
+++ environments/prod/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3"{
+        bucket       = "nj-2048-tfstate"
+        key          = "prod/terraform.tfstate"
+        region       = "ap-south-1"
+        encrypt      = true
+        use_lockfile = true
+    }
+}
