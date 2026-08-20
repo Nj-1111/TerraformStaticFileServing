fe1233deb7cd9eb579200866eca20deba6219034
Neel Bose <neelbosen.jr11@gmail.com>
2026-08-20 13:26:23 +0000

    change the cicd main.tf file for a easy fix of pointing the repo:Nj-1111*/2048BoardGame-*

--- environments/cicd/main.tf
+++ environments/cicd/main.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+            "token.actions.githubusercontent.com:sub" = "repo:Nj-1111*/2048-BoardGame-*:*"
           }
         }
       }
